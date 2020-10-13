5f802451e3f118a919f074ac0f08dc779d1e0a42
unknown <vladtimofte999@gmail.com>
2020-10-13 16:10:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function openComponent(evt, component) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(component).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  /*counter*/
+  $('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
